7910216d72cd95ace6d0b5eb067636aac242f701
Coleman Kane <cdkane9@gmail.com>
2025-03-07 16:43:06 -0700

    changed function calls, added pit id as argument

--- doing_everything.py
+++ doing_everything.py
@@ -1,12 +1,17 @@
 import pandas as pd
 import numpy as np
 from pits import pit_scrubber
+from ram import ram_scrubber
+from matrix import matrix_scrubber
 
-pit_path = '/Users/colemankane/Documents/BSU/CRREL Snow Strength/snow_strength_cleaning/Freeman/20250122/FRE_20250122_pit.xlsx'
-pit_scrubber(pit_path)
+id = 'FRE_20250122'
 
+pit_path = f'/Users/colemankane/Documents/BSU/CRREL Snow Strength/snow_strength_cleaning/Freeman/20250122/{id}_pit.xlsx'
+pit_scrubber(pit_path, id)
 
+ram_path = f'/Users/colemankane/Documents/BSU/CRREL Snow Strength/snow_strength_cleaning/Freeman/20250122/{id}_sram.xlsx'
+ram_scrubber(id, ram_path)
 
-"""
-Nothing here yet... 
-"""
+mat_path = f'/Users/colemankane/Documents/BSU/CRREL Snow Strength/snow_strength_cleaning/Freeman/20250122/{id}_matrix.xlsx'
+matrix_scrubber(mat_path, id)
+
