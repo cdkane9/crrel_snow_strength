5e1667986b2bb516f6cff9b1200697d99066ce3d
Coleman Kane <cdkane9@gmail.com>
2025-03-07 14:53:39 -0700

    added function calls to test pits.py

--- doing_everything.py
+++ doing_everything.py
@@ -1,5 +1,10 @@
 import pandas as pd
 import numpy as np
+from pits import pit_scrubber
+
+pit_path = '/Users/colemankane/Documents/BSU/CRREL Snow Strength/snow_strength_cleaning/Freeman/20250122/FRE_20250122_pit.xlsx'
+pit_scrubber(pit_path)
+
 
 
 """
