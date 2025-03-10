b6ac41861aad963a1911b5f619da5960c3bfb498
Coleman Kane <cdkane9@gmail.com>
2025-03-10 09:50:15 -0600

    added site, date, as variables

--- doing_everything.py
+++ doing_everything.py
@@ -4,14 +4,16 @@ from pits import pit_scrubber
 from ram import ram_scrubber
 from matrix import matrix_scrubber
 
-id = 'FRE_20250122'
+site = 'FRE'
+date = '20241210'
+id = f'{site}_{date}'
 
-pit_path = f'/Users/colemankane/Documents/BSU/CRREL Snow Strength/snow_strength_cleaning/Freeman/20250122/{id}_pit.xlsx'
+pit_path = f'/Users/colemankane/Documents/BSU/CRREL Snow Strength/snow_strength_cleaning/Freeman/{date}/{id}_pit.xlsx'
 pit_scrubber(pit_path, id)
 
-ram_path = f'/Users/colemankane/Documents/BSU/CRREL Snow Strength/snow_strength_cleaning/Freeman/20250122/{id}_sram.xlsx'
+ram_path = f'/Users/colemankane/Documents/BSU/CRREL Snow Strength/snow_strength_cleaning/Freeman/{date}/{id}_sram.xlsx'
 ram_scrubber(id, ram_path)
 
-mat_path = f'/Users/colemankane/Documents/BSU/CRREL Snow Strength/snow_strength_cleaning/Freeman/20250122/{id}_matrix.xlsx'
+mat_path = f'/Users/colemankane/Documents/BSU/CRREL Snow Strength/snow_strength_cleaning/Freeman/{date}/{id}_matrix.xlsx'
 matrix_scrubber(mat_path, id)
 
