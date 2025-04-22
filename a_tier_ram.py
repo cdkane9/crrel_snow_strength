a2b1d12f259d18373ee101eef0ab45eb4dc295ea
Coleman Kane <cdkane9@gmail.com>
2025-04-22 12:58:32 -0600

    added error bars

--- a_tier_ram.py
+++ a_tier_ram.py
@@ -13,24 +13,31 @@ data_lst = os.listdir(data_dir)
 #comp = pd.DataFrame(columns=['max_force', 'max_rr'])
 
 frams = [i for i in data_lst if i.endswith('fram.csv')]
-rcomp = np.zeros((len(frams), 2))
+results = []
 
 for f in range(len(frams)):
     fram = pd.read_csv(data_dir + '/' + frams[f])
     if not fram.empty:
-        max_force = np.nanmean(fram['Max Force'])
-        max_rr = np.nanmean(fram['max_rr'])
-        #comp = pd.concat([comp, pd.DataFrame({'max_force': max_force, 'max_rr': max_rr})])
-        rcomp[f, 0] = max_force
-        rcomp[f, 1] = max_rr
-    else:
-        pass
-force = rcomp[:, 0].reshape(-1, 1)
-rr = rcomp[:, 1]
-f_out_ix = rr < 400
+        max_force = list(fram['Max Force'])
+        max_rr = list(fram['max_rr'])
+        id = list(fram['id'])
+        for i in range(len(max_force)):
+            results.append([max_rr[i], max_force[i], id[i]])
 
-force_out = force[f_out_ix]
-rr_out = rr[f_out_ix]
+
+
+comp = pd.DataFrame(results, columns=['rr', 'force', 'id'])
+comp = comp[comp['rr'] <= 400]
+comp['rr'] = comp['rr'].astype(float)
+comp['force'] = comp['force'].astype(float)
+
+
+grouped = comp.groupby('id')[['rr', 'force']]
+means = grouped.mean()
+stds = grouped.std()
+
+avg_rr = np.array(means['rr']).reshape(-1,1)
+avg_force = np.array(means['force'])
 
 def rmse(actual, predicted):
     error = actual - predicted
@@ -39,25 +46,50 @@ def rmse(actual, predicted):
     return root
 
 model = LinearRegression(fit_intercept=False)
-
-model.fit(force_out, rr_out)
-
+model.fit(avg_rr, avg_force)
 m = model.coef_[0]
+root_error = rmse(means['force'], means['rr'] * m)
 
-root_error = rmse(rr_out, force_out * m)
-r2 = (sklearn.metrics.r2_score(rr_out, force_out * m))
 
-x = np.linspace(0, 200, len(rr))
-plt.xlabel('Max Force (N)', fontsize=20)
-plt.ylabel('Max RR (N)', fontsize=20)
-plt.scatter(force_out, rr_out, color = 'black')
-plt.plot(x, m * x, color='darkslategrey', label=f'y = {round(m, 2)}x, \n'
-                                                f'RMSE = {round(root_error, 2)}, \n'
-                                                f'R2 = {round(r2, 2)}')
+r2 = (sklearn.metrics.r2_score(means['force'], means['rr'] * m))
+
+
+
+x = np.linspace(0, 400, len(means['rr']))
+plt.scatter(means['rr'], means['force'], color='black')
+plt.errorbar(means['rr'], means['force'],
+             xerr=stds['rr'], yerr=stds['force'],
+             fmt='o', capsize=3, elinewidth=2, markeredgewidth=0.5, color='black')
+plt.plot(x, x * m, color='darkslategray',
+         label = f'y = {round(m, 2)}x \n'
+         f'RMSE = {round(root_error, 2)} \n'
+         f'R2 = {round(r2, 2)}')
+plt.xlabel('Ram Resistance (N)', fontsize=20)
+plt.ylabel('Force (N)', fontsize=20)
 plt.legend(fontsize=16)
 plt.tight_layout()
 plt.show()
 
+'''
+model.fit(max_rr.reshape(-1, 1), max_force)
+
+m = model.coef_[0]
+
+root_error = rmse(max_force, max_rr * m)
+r2 = (sklearn.metrics.r2_score(max_force, max_rr * m))
+
+x = np.linspace(0, 1300, len(max_rr))
+plt.xlabel('Max RR (N)', fontsize=20)
+plt.ylabel('Max Force (N)', fontsize=20)
+plt.scatter(max_rr, max_force, color = 'black')
+plt.plot(x, m * x, color='darkslategrey', label=f'y = {round(m, 2)}x \n'
+                                                f'RMSE = {round(root_error, 2)} \n'
+                                                f'R2 = {round(r2, 2)}')
+#plt.plot(x, 1 * x, linestyle=':')
+plt.legend(fontsize=16)
+plt.tight_layout()
+plt.show()
+'''
 
 
 
