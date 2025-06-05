5778817646fdf254bc9f20678499e0530d75f062
Coleman Kane <cdkane9@gmail.com>
2025-06-05 10:59:22 -0600

    initial commit for cleaning just brundage data

---
+++ bdg_cleaning.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+file_path = '/Users/colemankane/Desktop/crrel_exports/wx_stations/Brundage_15min_dirty.csv'
+caca = pd.read_csv(file_path)
+
+
+
