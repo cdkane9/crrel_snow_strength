6b42c59a0cb5c012b899f83db12fdb5a785cb5f1
Coleman Kane <cdkane9@gmail.com>
2025-08-13 15:50:51 -0600

    updated with better density estimates

--- smp_density.py
+++ smp_density.py
@@ -6,13 +6,13 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 
 data_dir = '/Users/colemankane/Library/CloudStorage/GoogleDrive-ColemanKane@boisestate.edu/Shared drives/2024-2025 CRREL Snow Strength/Data/Scrubbed pit strength transect data/crrel_exports'
-smp_dir = '/Users/colemankane/Desktop/crrel_exports/smp_profiles'
+smp_dir = '/Users/colemankane/Library/CloudStorage/GoogleDrive-ColemanKane@boisestate.edu/Shared drives/2024-2025 CRREL Snow Strength/Data/Scrubbed pit strength transect data/crrel_exports/smp_profiles_exports'
 
 id_sites = ['FRE', 'BOG', 'BDG', 'PLT1', 'PLT2']
 strat_files = [i for i in os.listdir(data_dir) if i.endswith('_strat.csv')]
 strat_files = [i for i in strat_files if i.startswith(tuple(id_sites))]
 
-for file in strat_files:#[-2:-1]:
+for file in strat_files:
     print(file)
     id = f'{file.split('_')[0]}_{file.split('_')[1]}'
     smp_filename = id + '_smp.csv'
@@ -42,11 +42,13 @@ for file in strat_files:#[-2:-1]:
 
 
         for i in strat_filtered.index:
-
+        #for i in strat.index:
             rho_to_avg = []
 
             top = strat_filtered.loc[i, 'top_cm']
+            #top = strat.loc[i, 'top_cm']
             bottom = strat_filtered.loc[i, 'bottom_cm']
+            #bottom = strat.loc[i, 'bottom_cm']
             delta = top - bottom
 
             top_depth = ((hs - top) + (0.2 * delta)) * 10
@@ -57,7 +59,7 @@ for file in strat_files:#[-2:-1]:
                 smp_prof = pd.read_csv(os.path.join(smp_dir, j), dtype=float)
                 smp_depth = smp_prof['distance [mm]'].iloc[-1]
                 smp_layer_ix = smp_prof.index[(smp_prof['distance [mm]'] >= top_depth) & (smp_prof['distance [mm]'] < bottom_depth)]
-                layer_rho = smp_prof.loc[smp_layer_ix]['P2015_density [kg/m^3]'] # all density values from layer
+                layer_rho = smp_prof.loc[smp_layer_ix]['CR2020_density [kg/m^3]'] # all density values from layer
                 if layer_rho.empty: #in cases where there are layers deeper than SMP
                     delta = np.nan
                     layer_swe = np.nan
@@ -67,15 +69,16 @@ for file in strat_files:#[-2:-1]:
                     rho_to_avg.append(avg_layer_rho)
 
             layer_density = np.nanmean(rho_to_avg) #average density for given layer
+            #print(layer_density)
             layer_swe = layer_density * delta / 100
 
             strat.at[i, 'hs_cm'] = round(delta,1) # fill values in stratigraphy file
             strat.at[i, 'swe_mm'] = round(layer_swe,1)
             strat.at[i, 'density_kgm3'] = round(layer_density,1)
 
+
+
         strat.to_csv(data_dir + '/' + file, index=False)
-
-
 
 
     except Exception as e:
