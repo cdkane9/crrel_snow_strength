65cdf4c3c8daf9b8752937bcd208738533eeec5b
Coleman Kane <cdkane9@gmail.com>
2025-08-13 13:57:12 -0600

    added comments

--- smp_density.py
+++ smp_density.py
@@ -18,19 +18,20 @@ for file in strat_files:#[-2:-1]:
     smp_filename = id + '_smp.csv'
 
     try:
-        smp_mat = pd.read_csv(os.path.join(data_dir, smp_filename))
-        good_profs = smp_mat['Comments'].isna()
-        good_profs = smp_mat['Y-Coord'] == -30
+        smp_mat = pd.read_csv(os.path.join(data_dir, smp_filename)) # read in file containing SMP profiles
+        good_profs = smp_mat['Comments'].isna() # filters out profiles with errors/overloads
+        good_profs = smp_mat['Y-Coord'] == -30 # use only profiles 30cm away from pit wall
         smp_mat = smp_mat[good_profs]
-        smp_sn = smp_mat['SN'].astype(int).astype(str).str.zfill(2)
+
+        smp_sn = smp_mat['SN'].astype(int).astype(str).str.zfill(2) # create filenames for SMP profiles
         smp_pn = smp_mat['Profile #'].astype(int).astype(str).str.zfill(4)
         smp_profs = 'S' + smp_sn + 'M' + smp_pn + '.PNT_derivatives.csv'
         smp_profs = list(smp_profs)
 
 
-        strat = pd.read_csv(data_dir + '/' + file)
+        strat = pd.read_csv(data_dir + '/' + file) # read in strat file
 
-        bottom = strat.index[strat['bottom_cm'] == 0][0]
+        bottom = strat.index[strat['bottom_cm'] == 0][0] # find the bottom of the stratigraphy profile
         strat = strat.loc[:bottom]
 
         hs = strat.loc[0, 'top_cm']
@@ -57,8 +58,10 @@ for file in strat_files:#[-2:-1]:
                 smp_depth = smp_prof['distance [mm]'].iloc[-1]
                 smp_layer_ix = smp_prof.index[(smp_prof['distance [mm]'] >= top_depth) & (smp_prof['distance [mm]'] < bottom_depth)]
                 layer_rho = smp_prof.loc[smp_layer_ix]['P2015_density [kg/m^3]'] # all density values from layer
-                if layer_rho.empty:
-                    rho_to_avg.append(np.nan)
+                if layer_rho.empty: #in cases where there are layers deeper than SMP
+                    delta = np.nan
+                    layer_swe = np.nan
+                    layer_density = np.nan
                 else:
                     avg_layer_rho = np.nanmean(layer_rho) # density of layer from one profile
                     rho_to_avg.append(avg_layer_rho)
@@ -66,11 +69,11 @@ for file in strat_files:#[-2:-1]:
             layer_density = np.nanmean(rho_to_avg) #average density for given layer
             layer_swe = layer_density * delta / 100
 
-            strat.at[i, 'hs_cm'] = delta
-            strat.at[i, 'swe_mm'] = layer_swe
-            strat.at[i, 'density_kgm3'] = layer_density
+            strat.at[i, 'hs_cm'] = round(delta,1) # fill values in stratigraphy file
+            strat.at[i, 'swe_mm'] = round(layer_swe,1)
+            strat.at[i, 'density_kgm3'] = round(layer_density,1)
 
-        strat.to_csv(data_dir + '/' + file)
+        strat.to_csv(data_dir + '/' + file, index=False)
 
 
 
